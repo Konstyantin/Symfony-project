d9a99f53e7f750b11d7e607f9984f52599ab37a1
kostya <kostya_nagula@mail.ua>
2017-10-10 21:46:18 +0300

    require and set configuration sonata admin bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,11 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
