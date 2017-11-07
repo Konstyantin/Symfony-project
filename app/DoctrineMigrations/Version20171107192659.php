59bbcd05eb5b86ca7b3efa7774bad10ddb68a703
kostya <kostya_nagula@mail.ua>
2017-11-07 21:28:55 +0200

    generate fuel entity

---
+++ app/DoctrineMigrations/Version20171107192659.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171107192659 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE fuel (id INT AUTO_INCREMENT NOT NULL, city INT NOT NULL, country INT NOT NULL, combined INT NOT NULL, emission INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE engine CHANGE car_drive car_drive ENUM(\'front\', \'full\', \'back\')');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE fuel');
+        $this->addSql('ALTER TABLE engine CHANGE car_drive car_drive VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci');
+    }
+}
---
+++ src/CarBundle/Entity/Fuel.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace CarBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Fuel
+ *
+ * @ORM\Table(name="fuel")
+ * @ORM\Entity(repositoryClass="CarBundle\Repository\FuelRepository")
+ */
+class Fuel
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     * @ORM\Column(name="city", type="integer")
+     */
+    protected $city;
+
+    /**
+     * @var integer
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     * @ORM\Column(name="country", type="integer")
+     */
+    protected $country;
+
+    /**
+     * @var integer
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     * @ORM\Column(name="combined", type="integer")
+     */
+    protected $combined;
+
+    /**
+     * @var integer
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     * @ORM\Column(name="emission", type="integer")
+     */
+    protected $emission;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set city
+     *
+     * @param integer $city
+     *
+     * @return Fuel
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return integer
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set country
+     *
+     * @param integer $country
+     *
+     * @return Fuel
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return integer
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set combined
+     *
+     * @param integer $combined
+     *
+     * @return Fuel
+     */
+    public function setCombined($combined)
+    {
+        $this->combined = $combined;
+
+        return $this;
+    }
+
+    /**
+     * Get combined
+     *
+     * @return integer
+     */
+    public function getCombined()
+    {
+        return $this->combined;
+    }
+
+    /**
+     * Set emission
+     *
+     * @param integer $emission
+     *
+     * @return Fuel
+     */
+    public function setEmission($emission)
+    {
+        $this->emission = $emission;
+
+        return $this;
+    }
+
+    /**
+     * Get emission
+     *
+     * @return integer
+     */
+    public function getEmission()
+    {
+        return $this->emission;
+    }
+}
