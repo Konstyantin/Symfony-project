2cdfbc0d6065a4f5e40f7d16de52a7876eccb81b
kostya <kostya_nagula@mail.ua>
2018-01-01 21:55:34 +0200

    show model list

---
+++ src/CarBundle/Services/ModelManagement.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kostya
+ * Date: 01.01.18
+ * Time: 20:32
+ */
+namespace CarBundle\Services;
+
+use Doctrine\ORM\EntityManager;
+
+/**
+ * Class ModelManagement
+ * @package CarBundle\Services
+ */
+class ModelManagement
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * ModelManagement constructor.
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Get model list
+     *
+     * @return mixed
+     */
+    public function getList() {
+        return $this->em->getRepository('CarBundle:Model')
+            ->getModelsList();
+    }
+}
