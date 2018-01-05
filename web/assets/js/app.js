907dc19f23e2ab569d057984c806ed73db108e52
Konstantin <kostyannagula@gmail.com>
2018-01-05 17:01:35 +0200

    show car on model list car

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -22,7 +22,8 @@
         menuHover: function () {
 
             var navList = $('.model-nav-list > li'),
-                modelItem = $('.model-item');
+                modelItem = navList.find('.model-item'),
+                modelCarItem = navList.find('.model-car-item');
 
             navList.on('mouseenter', function () {
                 navList.removeClass('open');
@@ -38,8 +39,23 @@
                    modelId = $this.attr('id');
 
                 $('.model-car-list').hide();
+                $('.item-car-description').hide();
 
                $('#car-id-' + modelId + '').show();
+            });
+
+            modelItem.on('mouseleave', function () {
+                $('.item-car-description').hide();
+            });
+
+            modelCarItem.on('mouseenter', function () {
+
+                var $this = $(this),
+                    carId = $this.attr('id');
+
+                $('.item-car-description').hide();
+
+                $('#car-description-id-' + carId + '').show();
             });
         }
     };
