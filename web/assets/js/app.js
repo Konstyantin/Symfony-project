a2d0980bd16683f44aece204ad7a7679f571d699
kostya <kostyannagula@gmail.com>
2018-02-21 01:33:46 +0200

    build offers category tree

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -14,6 +14,7 @@
         init: function () {
             this.menuHover();
             this.actionListManage();
+            this.offersTree();
         },
 
         /**
@@ -93,6 +94,64 @@
 
                 actionStory.slideToggle();
             })
+        },
+
+        /**
+         * Change display condition element
+         *
+         * @param menu
+         * @param subList
+         */
+        changeCondition: function (menu, subList) {
+
+            var that = this;
+            $.each(menu, function () {
+                var list = $(this).find('.dropdown-menu');
+
+                /**
+                 * If elem display remove class active
+                 */
+                if (that.checkActive(list)) {
+                    list.removeClass('active');
+                }
+            });
+
+            /**
+             * if menu show remove class active, if not add class active
+             */
+            subList.toggleClass('active');
+        },
+
+        /**
+         * Checks whether the element contains a class active
+         *
+         * @param elem
+         * @returns {boolean}
+         */
+        checkActive: function (elem) {
+            return elem.hasClass('active');
+        },
+
+
+        offersTree: function () {
+            var that = this;
+            $('.dropdown-submenu a.submenu').hover(function(e){
+
+                /**
+                 * Define variable
+                 */
+                var $this = $(this),
+                    menu = $this.closest('.dropdown-menu'),
+                    subList = $this.siblings('ul');
+
+                console.log(subList);
+
+                /**
+                 * Call method changeCondition
+                 */
+                that.changeCondition(menu, subList);
+
+            });
         }
     };
 
