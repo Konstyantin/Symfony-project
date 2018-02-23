3b42a301e5a7805b67fefba6a97c949f11c9830d
kostya <kostyannagula@gmail.com>
2018-02-24 01:03:19 +0200

    add dealer function test

---
+++ tests/functional/DealerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class DealerTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \FunctionalTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    /**
+     * Test dealer list
+     *
+     * Test show list of dealers
+     */
+    public function testDealerList()
+    {
+        $I = $this->tester;
+
+        $I->amOnPage('/dealer/list');
+        $I->see('Kharkiv Center', 'h3');
+        $I->see('Lviv Center', 'h3');
+        $I->see('Kiev Aueroport', 'h3');
+
+        $I->see('61000, Kharkiv, Pushkinska Street, 79/5', 'span.dealer-address');
+        $I->see('79000, Lviv, Shevchenka Street, 8', 'span.dealer-address');
+        $I->see('80000, Kiev, Kyivska Street, 43', 'span.dealer-address');
+        $I->seeNumberOfElements('div#map', 1);
+    }
+}
