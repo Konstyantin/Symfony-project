721a2f8e90fe0e18f7f2437815be1f19d9cc17e9
kostya <kostyannagula@gmail.com>
2018-02-27 23:20:09 +0200

    create acceptance test for model entity

---
+++ tests/acceptance/ModelCest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Class ModelCest
+ */
+class ModelCest
+{
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    /**
+     * Model list test
+     *
+     * @param AcceptanceTester $I
+     */
+    public function modelListToTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/model');
+
+        $I->see('Panamera');
+        $I->see('Macan');
+        $I->see('Cayenne');
+        $I->see('Panamera');
+        $I->see('911');
+        $I->see('718');
+
+        $I->see('Model details', 'h3');
+        $I->see('Porsche', 'ol.breadcrumb');
+    }
+
+    /**
+     * Model car test
+     *
+     * @param AcceptanceTester $I
+     */
+    public function modelCarTest(AcceptanceTester $I)
+    {
+        $I->am('model');
+
+        $I->see('911', '#911');
+        $I->click(['id' => '911']);
+
+        $I->seeCurrentUrlEquals('/model/911');
+        $I->see('911');
+    }
+}
