126e9b24e6761b49a487fb138de57a0fb1bf55c1
kostya <kostyannagula@gmail.com>
2018-03-09 17:23:33 +0200

    fixed problem associated with to choose users car and a model car

--- src/CarBundle/Repository/ModelRepository.php
+++ src/CarBundle/Repository/ModelRepository.php
@@ -54,6 +54,8 @@ class ModelRepository extends \Doctrine\ORM\EntityRepository
     {
         $query = $this->createQueryBuilder('model');
 
+        $userId = ($this->getUserModels($userId)) ? $userId : null;
+
         $query = ($userId) ? $query
             ->join('UserBundle:UserCar', 'user_car', 'WITH', 'model.id = user_car.model')
             ->where('user_car.user =:userId')
@@ -62,6 +64,25 @@ class ModelRepository extends \Doctrine\ORM\EntityRepository
         $query->orderBy('model.id', 'ASC');
 
         return $query;
+    }
+
+    /**
+     * Get user models
+     *
+     * Get models collection by passed user id param
+     *
+     * @param int $userId
+     * @return array
+     */
+    public function getUserModels(int $userId)
+    {
+        $query = $this->createQueryBuilder('model')
+            ->join('model.userCar', 'user_car')
+            ->where('user_car.user =:userId')
+            ->setParameter('userId', $userId)
+            ->getQuery();
+
+        return $query->getResult();
     }
 
     /**
