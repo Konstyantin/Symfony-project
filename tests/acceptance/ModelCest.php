93fcf7f7cd78c8f2069e3554b30f73ad8b8ab04e
kostya <kostyannagula@gmail.com>
2018-03-09 09:56:55 +0200

    create acceptance test case responsible for searching a model items

--- tests/acceptance/ModelCest.php
+++ tests/acceptance/ModelCest.php
@@ -38,8 +38,9 @@ class ModelCest
         $I->see('Model details', 'h3');
         $I->see('Porsche', 'ol.breadcrumb');
 
-        $I->click(['link' => 'Macan']);
+        $I->seeNumberOfElements('div.model-item', [5, 100]);
     }
+
 
     /**
      * Model car test
@@ -51,9 +52,40 @@ class ModelCest
         $I->amOnPage('/model');
 
         $I->see('911', '#911');
-        $I->click(['id' => '911']);
+        $I->click(['link' => '911']);
+
+        $I->dontSeeCurrentUrlEquals('/model/Caynne');
+        $I->dontSee('Cayenne');
 
         $I->seeCurrentUrlEquals('/model/911');
         $I->see('911');
+    }
+
+    /**
+     * Search model test
+     *
+     * @param AcceptanceTester $I
+     */
+    public function searchModelTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/model');
+
+        $I->seeElement('button.show-search-form-btn');
+
+        $I->click(['class' => 'show-search-form-btn']);
+
+        $I->seeElement('input.model-name-field');
+
+        $I->seeElement('form.search-model');
+
+        $I->submitForm('form.search-model', [
+            'car_bundle_model_search_type[name]' => '911'
+        ]);
+
+        $I->wait(2);
+        $I->dontSeeElement('a#Panamera');
+
+        $I->see('911', 'span');
+        $I->dontSee('Panamera', 'span');
     }
 }
